ba3e9e054ee72430b177b43f213c6b8e04e69b61
jsm4r1 <mariam.ozuna26@compscihigh.org>
2025-10-21 19:14:11 +0000

    create function requestUrl & fetch

--- index.js
+++ index.js
@@ -7,40 +7,48 @@
 //    - Convert the response to JSON
 //    - Log out the JSON or specific info (like name or country)
 
+let requestUrl = "https://randomuser.me/api/"
 
+async function fetchUserData(requestUrl) {
+    let response = await fetch(requestUrl)
+    let data = await response.json()
+    console.log(data)
+}
 
 // ----------------------------------------------------
 // Create a variable for each request below (reqUrl1, reqUrl2, etc.)
 // Then call your fetchUserData() function with each one.
 
+
 // 1 URL to Get 8 Random Users
 // Call the fetchUserData function with this URL
 
-
+let reqUrl1 =  requestUrl + "?results=8"
+fetchUserData(reqUrl1)
 
 // 2 URL to Get a Random User from Poland
 // Call the fetchUserData function with this URL
 
-
-
+let reqUrl2 = requestUrl + "?nat=ca"
+fetchUserData(reqUrl2)
 
 
 // 3 URL to Get a Random User that’s Male
 // Call the fetchUserData function with this URL
 
-
+let reqUrl3 = requestUrl + "?gender=male"
+fetchUserData(reqUrl3)
 
 
 // 4 URL to Get a Random User while Excluding Their Email
 // Call the fetchUserData function with this URL
 
-
-
+let reqUrl4 = requestUrl + "?exc=email"
+fetchUserData(reqUrl4)
 
 
 // 5 Get 5 Female Users from the Colombia
 // Call the fetchUserData function with this URL
 
-
-
-
+let reqUrl5 = requestUrl + "?gender=female&nat=fr&results=5"
+fetchUserData(reqUrl5)
